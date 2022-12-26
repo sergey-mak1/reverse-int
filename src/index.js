1df5a19dcd2b2cccf264a6599ec9adb3edd3c47f
sergey-mak1 <sega013021990@gmail.com>
2022-12-26 22:49:32 +0300

    feat: add function reverse in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+        let str;
+        let str2 = '';
+        str = String(Math.abs(n));
+        for (i = str.length - 1; i >= 0; i--) {
+            str2 += str[i];
+        }
+        return Number(str2);
+    }
+
